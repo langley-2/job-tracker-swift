2efe32af07f4fe10458b7a557923c1589aa66012
Langley <langleymillard@outlook.com>
2025-05-04 20:33:36 +1000

    Initial Commit

---
+++ job-tracker/job_trackerApp.swift
@@ -0,0 +1,32 @@
+//
+//  job_trackerApp.swift
+//  job-tracker
+//
+//  Created by Langley Millard on 4/5/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct job_trackerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ job-trackerTests/job_trackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  job_trackerTests.swift
+//  job-trackerTests
+//
+//  Created by Langley Millard on 4/5/2025.
+//
+
+import Testing
+@testable import job_tracker
+
+struct job_trackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
